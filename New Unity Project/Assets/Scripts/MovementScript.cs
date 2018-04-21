6fa857ef457ac664162dd9a243c50610bbc1cc32
Joel Sjöberg <joelsjoberg3@gmail.com>
2018-04-21 11:12:43 +0300

    Level1 in progress
    -Created Level1 background
    -Made simple movement script
    -Level blocks added for... level functionality

---
+++ New Unity Project/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("reg");
+            transform.position += new Vector3(1, 0, 0).normalized;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("reg");
+            transform.position += new Vector3(-1, 0, 0).normalized;
+        }
+    }
+
+
+}
