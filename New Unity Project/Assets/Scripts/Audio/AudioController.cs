75f5a63ef54c924256ce5c034bc8212f97b84710
Joel Sjöberg <joelsjoberg3@gmail.com>
2018-04-21 17:19:28 +0300

    Work has been done
    Animations have been implemented

---
+++ New Unity Project/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    AudioSource audio;
+
+    public Sound[] sounds;
+
+    public static AudioController instance;
+    private Sound currentSong;
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+
+    private void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.looping;
+        }
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void play(string name)
+    {
+        // find sound from sounds array to play
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- New Unity Project/Assets/Scripts/MovementScript.cs
+++ New Unity Project/Assets/Scripts/MovementScript.cs
@@ -4,41 +4,97 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
 
+
+    private string[] stepSounds = { "step1", "step2", "step3" };
+    private int stepCounter = 0;
+    private int[] sequence = {0, 1, 0, 2};
+    private int sequenceCounter = 0;
+    float lastUpdate = 0;
+    int inputCounter = 0;
+
+    Animator anim;
+    AudioController ac;
+    SpriteRenderer ren;
     public enum State
     {
+        idle,
         walkLeft,
         walkRight,
         jump,
-        inAir,
-        attack,
-
+        fall,
+        grab,
+        attack
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        ac = FindObjectOfType<AudioSource>().GetComponent<AudioController>();
+        anim = GetComponentInChildren<Animator>();
+        ren = GetComponentInChildren<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.D))
+        // in the future: find a better way to manipulate the animator! This is insane!!!
+        if(lastUpdate == 1)
         {
-            Debug.Log("reg");
-            transform.position += new Vector3(1, 0, 0).normalized;
+            anim.SetBool("NoInput", true);
+            
+            lastUpdate = 0;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.D) && inputCounter == 0)
         {
-            Debug.Log("reg");
+            ren.flipX = false;
+            transform.position += new Vector3(1, 0, 0).normalized;
+            playNext();
+            inputCounter++;
+            anim.SetBool("PressLR2", false);
+            anim.SetBool("PressLR", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && inputCounter == 1)
+        {
+            ren.flipX = false;
+            transform.position += new Vector3(1, 0, 0).normalized;
+            playNext();
+            inputCounter = 0;
+            anim.SetBool("PressLR", false);
+            anim.SetBool("PressLR2", true);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && inputCounter == 0)
+        {
+            ren.flipX = true;
             transform.position += new Vector3(-1, 0, 0).normalized;
+            playNext();
+            inputCounter++;
+            anim.SetBool("PressLR2", false);
+            anim.SetBool("PressLR", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && inputCounter == 1)
+        {
+            ren.flipX = true;
+            transform.position += new Vector3(-1, 0, 0).normalized;
+            playNext();
+            inputCounter = 0;
+            anim.SetBool("PressLR", false);
+            anim.SetBool("PressLR2", true);
         }
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
         {
-            Debug.Log("reg");
             transform.position += new Vector3(0, 1, 0).normalized;
+            playNext();
         }
-
+        lastUpdate += Time.deltaTime;
+    }
+    
+    // Play a 8-bit melody as the player moves, the solution is bad but it works.
+    public void playNext()
+    {
+        ac.play(stepSounds[stepCounter]);
+        sequenceCounter = (sequenceCounter + 1) % 4;
+        stepCounter = sequence[sequenceCounter];
     }
 
+    private int walkSequence = 0;
 
 }
