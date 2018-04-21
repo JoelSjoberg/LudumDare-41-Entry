c7c5732f00dce1fcb326e2334623c333295783d4
Joel Sjöberg <joelsjoberg3@gmail.com>
2018-04-21 12:38:26 +0300

    Scripts
    GameStateController will tell each entity when to execute behaviour

---
+++ New Unity Project/Assets/Scripts/GameStateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour {
+
+    // make all event execute at the same time, inputs are stored in a variable and executed when this class allows it.
+
+    public static bool execute = false;
+    public int operationsPerSec = 4;
+    private float totalTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        totalTime = (1/ operationsPerSec % totalTime == 1 / operationsPerSec) ? (totalTime = 0) : (totalTime += Time.deltaTime);
+        execute = totalTime == 0;
+        Debug.Log(execute);
+	}
+}
--- New Unity Project/Assets/Scripts/MovementScript.cs
+++ New Unity Project/Assets/Scripts/MovementScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
 
+    public enum State
+    {
+        walkLeft,
+        walkRight,
+        jump,
+        inAir,
+        attack,
+
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -12,16 +21,23 @@ public class MovementScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("reg");
             transform.position += new Vector3(1, 0, 0).normalized;
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("reg");
             transform.position += new Vector3(-1, 0, 0).normalized;
         }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("reg");
+            transform.position += new Vector3(0, 1, 0).normalized;
+        }
+
     }
 
 
